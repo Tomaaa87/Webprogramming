1f2c71cda267c02b7a463e68a730b151731d2f30
Tomaaa87 <daniel.tomic@stu.ibu.edu.ba>
2025-12-26 13:30:04 +0100

    valjda radi sada

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,6 +1,6 @@
 // Idempotent global constants (safe to include multiple times)
 window.Constants = window.Constants || {
-   PROJECT_BASE_URL: "https://carparts-app-4byte.ondigitalocean.app/",
+   PROJECT_BASE_URL: "https://carparts-app-4byte.ondigitalocean.app/webprogramming-backend/",
    USER_ROLE: "user",
    ADMIN_ROLE: "admin"
 };
